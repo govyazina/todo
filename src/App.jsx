a2cf3087b6b297d11ae478916612674d99d51dff
Svetlana Govyazina <govyazina@gmail.com>
2023-05-08 20:27:41 +0300

    sort for complited task added

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
                 el.taskStatus = !el.taskStatus
             }
             return {...el}
-        })
+        }).sort((a, b) => a.taskStatus - b.taskStatus)
         setTaskList(newArr)
     }
 
