bdc266509ea2d6d849db9c5a9d67e5263de23b2b
Svetlana Govyazina <govyazina@gmail.com>
2023-05-23 21:02:42 +0300

    styles for task list changed

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -19,21 +19,25 @@ export function Task({task, taskStatus, id}) {
     }
     return (
         <>
-            <label className="form-check-label" htmlFor="flexCheckDefault"
-                   style={taskStatus ? {textDecorationLine: 'line-through'} : {textDecorationLine: 'none'}}>
-                <input className="form-check-input"
-                       type="checkbox"
-                       id="flexCheckDefault"
-                       checked={taskStatus}
-                       onChange={() => changeTaskStatus(id)}/>
-                {task}
-            </label>
-            <button
-                type="button"
-                className="btn btn-secondary ms-2"
-                onClick={() => deleteTask(id)}>
-                Delete
-            </button>
+            <div className="container">
+                <div className="row">
+                    <label className="form-check-label col-3" htmlFor="flexCheckDefault"
+                           style={taskStatus ? {textDecorationLine: 'line-through'} : {textDecorationLine: 'none'}}>
+                        <input className="form-check-input"
+                               type="checkbox"
+                               id="flexCheckDefault"
+                               checked={taskStatus}
+                               onChange={() => changeTaskStatus(id)}/>
+                        {task}
+                    </label>
+                    <button
+                        type="button"
+                        className="btn btn-secondary col-1"
+                        onClick={() => deleteTask(id)}>
+                        Delete
+                    </button>
+                </div>
+            </div>
         </>
     )
 }
